cf539312369cb60e6814bdaf88919fdebffe8986
yuhaoyann <yuhaoyann@gmail.com>
2021-12-26 09:04:01 -0500

    set up file for testing db

--- src/environment.js
+++ src/environment.js
@@ -1,6 +1,7 @@
 const path = require("path");
 
 const ENV = process.env.NODE_ENV || "development";
+
 const PATH = path.resolve(__dirname, "../.env." + ENV);
 
 require("dotenv").config({ path: PATH });
